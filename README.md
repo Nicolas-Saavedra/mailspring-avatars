cb210e44259b9d686046d81c8f1d78c920fe4a6a
Nicolas-Saavedra <personal@nicolassaavedra.com>
2024-06-29 11:43:22 -0500

    feat: added naive "from tag" image source

--- README.md
+++ README.md
@@ -4,13 +4,18 @@ Simple, naive implementation of Avatars in the Message List.<br>
 Add avatar before each email you receive, no more, no less !<br>
 No caching, might do a lot of image requests.
 
-Fork of [emadow](https://github.com/emadow/mailspring-avatars) project, thanks for his work !<br>
+This fork was a personal modification, so it can base it's images on a few
+common regex patterns that the `From` tag might have, in case an alias
+overrides the regular email field. This is, also, a naive implementation,
+but it does the trick for now.
+
+Fork of [Striffly](https://github.com/Striffly/mailspring-avatars) project, thanks for his work !<br>
 
 ![Screenshot](screenshot.png)
 
 ## Installing
 
-1. [Grab the latest release](https://github.com/Striffly/mailspring-avatars/releases)
+1. [Grab the latest release](https://github.com/Nicolas-Saavedra/mailspring-avatars/releases)
 2. Extract Mailspring-Avatars and load Mailspring
 3. From the menu, select `Developer > Install a Package Manually...` from the dialog, choose the directory of this plugin to install it
 
--- src/avatar-factory.jsx
+++ src/avatar-factory.jsx
@@ -1,21 +1,84 @@
-import AvatarImage from './avatar-image';
-import { React } from 'mailspring-exports';
+import AvatarImage from "./avatar-image";
+import { React } from "mailspring-exports";
 
-export default function(size, name){
-  const Avatar = (props)=>{
-    const { thread={} } = props;
-    const { participants=[] } = thread;
-    if(!participants.length) return <noscript/>;
+const regularRedirect =
+  /^(.*) - ([a-zA-Z0-9._%+-]+\(a\)[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
 
-    let participant = participants[participants.length-1];
+const lowInfoRedirect = /^(.*) - ([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
 
-    if (participant.isMe() && participants.length>1) {
-      participant = participants[participants.length-2];
+const atRedirect = /([a-zA-Z0-9._%+-]+) at ([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+
+const atEscapedRedirect =
+  /^(.*) - '([a-zA-Z0-9._%+-]+) at ([a-zA-Z0-9.-]+\.[a-zA-Z]{2,})'$/;
+
+/**
+ * @param name {string}
+ */
+function obtainEmailAndName(name) {
+  let match = name.match(regularRedirect);
+  if (match) {
+    return [match[1], match[2].replace("(a)", "@")];
+  }
+  match = name.match(lowInfoRedirect);
+  if (match) {
+    return [match[1], `${match[1].split(" ")[0].toLowerCase()}@${match[2]}`];
+  }
+  match = name.match(atRedirect);
+  if (match) {
+    return [match[1], `${match[1]}@${match[2]}`];
+  }
+  match = name.match(atEscapedRedirect);
+  if (match) {
+    return [match[1], `${match[2]}@${match[3]}`];
+  }
+  return [null, null];
+}
+
+export default function (size, name) {
+  const Avatar = (props) => {
+    const { thread = {} } = props;
+    const { participants = [] } = thread;
+    const { __messages = {} } = thread;
+
+    if (!participants.length) return <noscript />;
+
+    let participant = participants[participants.length - 1];
+
+    if (participant.isMe() && participants.length > 1) {
+      participant = participants[participants.length - 2];
     }
 
-    if(!participant) return <noscript/>;
-    return <AvatarImage email={participant.email} name={participant.name} className='thread-avatar' size={size} round={true}/>
-  }
-  Avatar.displayName=name;
+    if (!participant) return <noscript />;
+
+    if (__messages.length > 0) {
+      const rawName = __messages[0].from[0].name;
+      if (rawName) {
+        const [name, email] = obtainEmailAndName(rawName);
+        if (name && email) {
+          // console.log(`Matched ${name} with email ${email}`);
+          return (
+            <AvatarImage
+              email={email}
+              name={name}
+              className="thread-avatar"
+              size={size}
+              round={true}
+            />
+          );
+        }
+      }
+    }
+    // console.log(`Did not match ${participant.name} with email ${participant.email}`,);
+    return (
+      <AvatarImage
+        email={participant.email}
+        name={participant.name}
+        className="thread-avatar"
+        size={size}
+        round={true}
+      />
+    );
+  };
+  Avatar.displayName = name;
   return Avatar;
 }
